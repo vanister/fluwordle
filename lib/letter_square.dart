9a2099d195201297707541a469eb58a4fa63f94c
vanister <vanister@users.noreply.github.com>
2022-03-01 21:41:48 -0800

    Added a letter square widget
    Signed-off-by: vanister <vanister@users.noreply.github.com>

---
+++ lib/letter_square.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class LetterSquare extends StatelessWidget {
+  final String letter;
+  final bool isCorrect;
+  final bool isClose;
+
+  const LetterSquare(this.letter,
+      {Key? key, this.isCorrect = false, this.isClose = false})
+      : super(key: key);
+
+  Color _colorSquare() {
+    if (isCorrect) {
+      return Colors.green.shade600;
+    }
+
+    if (isClose) {
+      return Colors.yellow.shade800;
+    }
+
+    return Colors.grey.shade900;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(2),
+      width: 50,
+      height: 50,
+      decoration:
+          BoxDecoration(border: Border.all(width: 4), color: _colorSquare()),
+      child: Center(
+          child: Text(
+        letter,
+        style: const TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)
+      )),
+    );
+  }
+}
