df8631c55be03009b6696c3a9515005cf4abb1cf
vanister <vanister@users.noreply.github.com>
2022-03-03 07:11:19 -0800

    Added a keyboard row
    Signed-off-by: vanister <vanister@users.noreply.github.com>

---
+++ lib/keyboard/keyboard_row.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:wordle_clone/models/key_data.dart';
+import 'package:wordle_clone/keyboard/keyboard_key.dart';
+
+class KeyboardRow extends StatefulWidget {
+  final List<KeyData> rowKeys;
+
+  const KeyboardRow(this.rowKeys, {Key? key}) : super(key: key);
+
+  @override
+  State<KeyboardRow> createState() => _KeyboardRowState();
+}
+
+class _KeyboardRowState extends State<KeyboardRow> {
+  late List<KeyData> _keys;
+
+  @override
+  void initState() {
+    super.initState();
+    // take a copy of the inital key row so we can update the key colors
+    // when a guess is made
+    _keys = [...widget.rowKeys];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: _keys
+            .map((k) => Padding(
+                  // pad they keys except the last key
+                  padding: EdgeInsets.only(right: k.lastKey ? 0.0 : 6.0),
+                  child: KeyboardKey(
+                    k.letter,
+                    isUsed: k.isUsed,
+                  ),
+                ))
+            .toList());
+  }
+}
